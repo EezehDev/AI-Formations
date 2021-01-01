3986c34ba24a61221d88ef32270cf6ce180d4fff
Wouter De Moor <mreezeh@gmail.com>
2021-01-01 20:06:52 +0100

    updated unreal project with camera movement and start of selection box

--- Formations/Source/Formations/Formations.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Formations.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Formations, "Formations" );
-
-DEFINE_LOG_CATEGORY(LogFormations)
- 
