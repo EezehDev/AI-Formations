d70a37e05a38b32e09a46e7969b1070be2fc1940
Wouter De Moor <mreezeh@gmail.com>
2021-01-18 11:22:16 +0100

    added particle when rightclicking

---
+++ Formations/Assets/Scripts/Player Controller/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private GameObject m_MoveParticle = null;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(1))
+        {
+            // Get mouse location
+            Vector3 mouseLocation = Input.mousePosition;
+            mouseLocation.z = Camera.main.transform.position.y;
+
+            // Convert mouse location to world space
+            Vector3 worldLocation = Camera.main.ScreenToWorldPoint(mouseLocation);
+
+            GameObject go = Instantiate(m_MoveParticle, worldLocation, Quaternion.identity);
+            Destroy(go, 1f);
+        }
+    }
+}
